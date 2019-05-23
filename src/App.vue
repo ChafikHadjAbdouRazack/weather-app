5be7bfe2887167c445eb6461745058f07f0f396e
amilamen <ezchilamen@gmail.com>
2019-05-23 01:34:47 +0000

    Customize ionic component and control input

--- src/App.vue
+++ src/App.vue
@@ -22,5 +22,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  background: url('assets/weather_ground.jpg') no-repeat center;
 }
 </style>
--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -1,38 +1,38 @@
 <template>
     <div class="weather">
         <form v-on:submit.prevent="getWeather">
-            <ion-grid>
-              <ion-row color="primary" justify-content-center>
-                <ion-col align-self-center size-md="6" size-lg="5" size-xs="12">
-                  <div padding>
-                      <ion-item>
-                          <ion-input type="text" name="city" placeholder="Enter your city name"></ion-input>
+            <ion-grid style="background: transparent;">
+              <ion-row style="background: transparent;" color="primary" justify-content-center>
+                <ion-col style="background: transparent;" align-self-center size-md="6" size-lg="5" size-xs="12">
+                  <div padding style="background: transparent;">
+                      <ion-item style="--background: transparent;">
+                          <ion-input class="item-highlight item-inner-highlight" style="--background: transparent; color: white; font-weight: bolder; font-size: larger;" @ionChange="getCity" type="text" name="city" placeholder="Enter your city name"></ion-input>
                       </ion-item>
                   </div>
-                   <ion-button color="primary" type="submit">Get it</ion-button>
-                </ion-col>
+                   <ion-button style="background: transparent; font-weight: bolder;" @click="presentLoading" v-bind:disabled="isDisabled" color="success" type="submit">Get it</ion-button>
+                </ion-col >
               </ion-row>
             </ion-grid>
         </form>
-       <ion-grid>
-           <ion-row color="primary" justify-content-center>
-              <ion-col align-self-center size-md="6" size-lg="5" size-xs="12">
-                 <ion-card>
-                     <ion-card-header>
-                        <ion-card-title>{{ country && city ? city+', '+country : 'No country' }}</ion-card-title>
+       <ion-grid style="background: transparent;">
+           <ion-row style="background: transparent;" color="primary" justify-content-center>
+              <ion-col style="background: transparent;" align-self-center size-md="6" size-lg="5" size-xs="12">
+                 <ion-card style="background: transparent; box-shadow: 0 13px 26px 58px rgba(0,0,0,.2), 0 16px 53px 0 rgba(0,0,0,.14), 0 1px 5px 0 rgba(0,0,0,.12); -webkit-box-shadow:  0 13px 26px 58px rgba(0,0,0,.2), 0 16px 53px 0 rgba(0,0,0,.14), 0 1px 5px 0 rgba(0,0,0,.12);">
+                     <ion-card-header style="background: transparent;">
+                        <ion-card-title style="color: white; font-weight: bolder;">{{ country && city ? city+', '+country : 'No country' }}</ion-card-title>
                      </ion-card-header>
-                     <ion-item lines="none" text-center>
-                        <ion-label>{{ weatherDescription ? weatherDescription : '' }}</ion-label>
-                     </ion-item>
-                     <ion-item style="display: block!important; margin: 0 auto!important;" lines="none">
-                        <ion-img  :src="icon" v-if="icon" alt="Weather Icon"></ion-img>
-                        <ion-label>{{ temp ? temp+'°C' : ''}}</ion-label>
-                     </ion-item>
-                     <ion-item line="none">
-                        <ion-label>{{ tempMin ? tempMin+'°C' : '' }}</ion-label>
-                        <ion-label>{{ tempMax ? tempMax+'°C' : '' }}</ion-label>
-                        <ion-label>{{ humidity ? 'Humidity: '+humidity+'%' : '' }}</ion-label>
-                     </ion-item>
+                        <ion-item style="--background: transparent;" lines="none" text-center>
+                            <ion-label style="--background: transparent; color: white; font-weight: bolder;">{{ weatherDescription ? weatherDescription : '' }}</ion-label>
+                        </ion-item>
+                        <ion-item style="display: -webkit-inline-box !important; margin: 0 auto!important; --background: transparent;" lines="none">
+                            <ion-img  :src="icon" v-if="icon" alt="Weather Icon"></ion-img>
+                            <ion-label style="--background: transparent; color: white; font-weight: bolder;">{{ temp ? temp+'°C' : ''}}</ion-label>
+                        </ion-item>
+                        <ion-item style="--background: transparent;" line="none">
+                            <ion-label style="--background: transparent; color: white; font-weight: bolder;">{{ tempMin ? tempMin+'°C' : '' }}</ion-label>
+                            <ion-label style="--background: transparent; color: white; font-weight: bolder;">{{ tempMax ? tempMax+'°C' : '' }}</ion-label>
+                            <ion-label style="--background: transparent; color: white; font-weight: bolder;">{{ humidity ? 'Hum: '+humidity+'%' : '' }}</ion-label>
+                        </ion-item>
                  </ion-card>
               </ion-col>
            </ion-row>
@@ -54,28 +54,68 @@
                 tempMin:'',
                 tempMax:'',
                 humidity:'',
-                API_KEY:'d63783958914771e7b1abff1566d9001'
+                API_KEY:'YOUR_API_KEY',
+                isDisabled: true,
+                timeout: 3000
             }
         },
         methods:{
             getWeather(event){
                 this.city=event.target[0].value
-                axios.get('https://api.openweathermap.org/data/2.5/weather?q='+this.city+'&mode=json&units=metric&appid='+this.API_KEY).then(response=>{
-                    console.log(response.data)
-                    let data =response.data
-                    this.country = data.sys.country
-                    this.temp = data.main.temp
-                    this.tempMin = data.main.temp_min
-                    this.tempMax = data.main.temp_max
-                    this.humidity = data.main.humidity
-                    this.weatherDescription = data.weather[0].description
-                    this.icon = 'https://openweathermap.org/img/w/'+data.weather[0].icon+'.png'
-                })
-            }
-        }
+                if(this.city.trim() !== '')
+                {
+                    axios.get('https://api.openweathermap.org/data/2.5/weather?q='+this.city+'&mode=json&units=metric&appid='+this.API_KEY).then(response=>{
+                        console.log(response.data)
+                        let data =response.data
+                        this.country = data.sys.country
+                        this.temp = data.main.temp
+                        this.tempMin = data.main.temp_min
+                        this.tempMax = data.main.temp_max
+                        this.humidity = data.main.humidity
+                        this.weatherDescription = data.weather[0].description
+                        this.icon = 'https://openweathermap.org/img/w/'+data.weather[0].icon+'.png'
+                    })
+                }
+            },
+           getCity(event)
+           {
+               console.log(event)
+               event.detail.value.trim() !== '' ? this.isDisabled = false : this.isDisabled = true
+           },
+            presentLoading()
+            {
+                return this.$ionic.loadingController.create({
+                        message: 'Loading',
+                        duration: this.timeout,
+                        spinner: 'bubbles',
+                        cssClass: 'loading-class'
+                    })
+                    .then(l => {setTimeout(function() {
+                                    l.dismiss()
+                                }, this.timeout)
+                        return l.present()
+                    })
+            },
+
+    }
     }
 </script>
 
 <style scoped>
+    .weather{
+        background: url('../assets/weather_ground.jpg') no-repeat center;
+        margin-top: -4%;
+    }
 
+    .item-highlight, .item-inner-highlight {
+        left: 0;
+        right: 0;
+        bottom: 0;
+        position: absolute;
+        --background: green
+    }
+
+    .loading-class{
+        --spinner-color: green;
+    }
 </style>
